32914d2479ec5a2e923520d96068e543b9957152
surajbelbase <89010692+surajbelbase@users.noreply.github.com>
2021-09-11 00:55:14 +0530

    Create add_two_num.cpp

---
+++ beginner_c++/add_two_num.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int T;
+    int a,b;
+    int c[T];
+    cin>>T;
+    for(int i = 0;i<T;i++){
+        cin>>a>>b;
+        cout<<a+b<<endl;
+        
+    }
+    return 0;
+}
