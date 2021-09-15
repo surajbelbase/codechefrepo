e259704c03fd7e92305f357acfc3c46fc9bccfe4
surajbelbase <89010692+surajbelbase@users.noreply.github.com>
2021-09-15 22:31:13 +0530

    Create number_mirror.cpp

---
+++ beginner_c++/number_mirror.cpp
@@ -0,0 +1,14 @@
+//question source: codechef(practice and learn - beginners)
+
+//problem : 4 (number mirror)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a;
+	cin>>a;
+	cout<<a;
+	return 0;
+}
