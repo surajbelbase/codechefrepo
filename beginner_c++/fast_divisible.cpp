28c8368e5b4eae97f953d02557c798c732cfcce2
surajbelbase <89010692+surajbelbase@users.noreply.github.com>
2021-09-15 22:27:41 +0530

    Update fast_divisible.cpp

--- beginner_c++/fast_divisible.cpp
+++ beginner_c++/fast_divisible.cpp
@@ -5,6 +5,10 @@
 // fast to handle problems branded with the enormous Input/Output warning. You are expected to be able to process 
 // at least 2.5MB of input data per second at runtime.
 
+// INPUT :   The input begins with two positive integers n k (n, k<=107). The next n lines of input contain one positive integer ti, not greater than 109, each.
+// OUTPUT :  Write a single integer to output, denoting how many integers ti are divisible by k.
+
+
 #include<iostream>
 using namespace std;
 
