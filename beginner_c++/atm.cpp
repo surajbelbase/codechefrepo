556c047aeefb5e94bc8ef18cb9cc9acf22503337
surajbelbase <89010692+surajbelbase@users.noreply.github.com>
2021-09-15 22:24:42 +0530

    Update atm.cpp

--- beginner_c++/atm.cpp
+++ beginner_c++/atm.cpp
@@ -1,4 +1,9 @@
-#include <iostream>
+//question source: codechef(practice and learn - beginners)
+
+
+//Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction if X is a multiple of 5, and Pooja's
+  //  account balance has enough cash to perform the withdrawal transaction (including bank charges). For each successful withdrawal the 
+  //  bank charges 0.50 $US. Calculate Pooja's account balance after an attempted transaction.#include <iostream>
 #include <iomanip>
 using namespace std;
 
